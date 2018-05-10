68e1d9294015d66876befafa2041882b66a7681e
Kurt Zikaras <kurtzikaras@gmail.com>
2018-05-09 21:42:03 -0400

    initial commit

---
+++ jamieocontractor/main.js
@@ -0,0 +1,20 @@
+var images = [
+    {url:""},
+    {url:""},
+    {url:""}
+];
+
+var photoGallery = $('#gallery');
+
+console.log(images.length);
+
+// images.forEach(function(){
+//     if(images.length <= 4){
+//         photoGallery.append(
+//             '<div class="row"><div class="card col-md-3" style="width: 18rem;"><img class="card-img-top" src="assets/images/houseOne.jpg" alt="Card image cap"><div class="card-body"><p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p></div></div></div>'
+//         );
+//     }
+// });
+
+
+
